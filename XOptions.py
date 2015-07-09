ca2905f79e2a7460c0e8199a60cdf21cddd9930a
root <root@rahu.parwal>
2015-07-09 22:21:27 +0000

    New

--- XOptions.py
+++ XOptions.py
@@ -10,7 +10,7 @@ def a():
 	os.system("dialog --no-shadow --infobox '	    Welcome to the X-Options  ' 10 40") 
 	time.sleep(2)
 	def x():
-		os.system("dialog --no-shadow --title 'X-Options' --menu 'Select Your Choice : ' 20 100 7 1 'Use Terminal inside the Browser ' 2 'Create QR Code ' 3 'Read QR Code ' 4 'Compress a File.' 5 'Decompress a File.' 6 'Back' 2> /root/Desktop/dump/choice")
+		os.system("dialog --no-shadow --title 'X-Options' --menu 'Select Your Choice : ' 20 100 7 1 'Use Terminal inside the Browser ' 2 'Use your System(s) Linux Terminal in your Smart Phone ' 3 'Create QR Code ' 4 'Read QR Code ' 5 'Compress a File.' 6 'Decompress a File.' 7 'Back' 2> /root/Desktop/dump/choice")
 		f=open('/root/Desktop/dump/choice','r')
 		c=f.read()
 		if c=='1':
@@ -25,8 +25,21 @@ def a():
 			os.system("service shellinabox restart")
 			os.system("firefox "+addr)
 			x()
-
+		
 		if c=='2':
+			ip=commands.getoutput("ifconfig | grep Bcast | awk '{print $2}' | cut -c 6-18")
+			f=open('/root/Desktop/dump/ip','w')
+			f.write("https://")			
+			f.write(ip)
+			f.write(":4200")
+			f.close()
+			os.system("service shellinabox start > /dev/null")
+			os.system("qrencode -s 12*12 -o /root/Desktop/dump/terminal.png < /root/Desktop/dump/ip")
+			os.system("dialog --no-shadow --msgbox 'Read the QR code with the QR code reader of your Smartphone' 10 50")
+			os.system("display /root/Desktop/dump/terminal.png")
+			x()
+
+		if c=='3':
 			os.system("dialog --no-shadow --inputbox 'Enter the data or link that you want to convert to QR code' 10 30 2> /root/Desktop/dump/qr")
 			rand=random.randint(1,1000)
 			r=str(rand)
@@ -35,7 +48,7 @@ def a():
 			os.system("display /root/Desktop/"+r+"qrcode.png")
 			x()
 
-		if c=='3':
+		if c=='4':
 			os.system("dialog --no-shadow --inputbox 'Enter the path of the QR coded File :' 10 50 2> /root/Desktop/dump/dqr")
 			f=open('/root/Desktop/dump/dqr','r')
 			link=f.read()
@@ -43,10 +56,10 @@ def a():
 			os.system("dialog --no-shadow --textbox /root/Desktop/dump/decode 10 30")
 			x()
 
-		if c==4:
+		if c==5:
 			exit()
 
 
 	x()
 
-
+a()
--- infogather.py
+++ infogather.py
@@ -3,30 +3,61 @@
 import os
 import time
 import commands
+import signal
 
 def a():
+
+	def my(x,y):
+		os.system("dialog --no-shadow --infobox 'Downloading Stopped.\nThank You.' 10 40")
+		time.sleep(1)
+		os.system("killall wget")	
+		info()
+  
+
+	signal.signal(2,my)
+
+
 	os.system("dialog --no-shadow --infobox ' Welcome to the Reconnaissance tool' 10 40") 
 	time.sleep(2)
-	os.system("dialog --no-shadow --menu 'Select Your Choice : ' 20 100 7 1 'Gather information about any website and it(s) owner.' 2 'Know the route to the server where website is hosted.' 3 'Back ' 2> /root/Desktop/Project/choice.txt")
-	f=open('/root/Desktop/Project/choice.txt','r')
-	c=f.read()
-	if c=='1':
-		os.system("dialog --no-shadow --inputbox 'Enter website name : (Example : google.com) ' 10 50 2> /root/Desktop/Project/hello.txt")
-		f=open('/root/Desktop/Project/hello.txt','r')
-		x=f.read()
-		os.system("dialog --no-shadow --infobox '  Fetching Data.. ' 10 40")
-		os.system("whois -H "+x+" > /root/Desktop/dump/info" +x)
-		os.system("sed -i 's/WHOIS/master/g' /root/Desktop/dump/info"+x)
-		os.system("sed -i 's/whois/master/g' /root/Desktop/dump/info"+x)
-		os.system("dialog --no-shadow --textbox /root/Desktop/dump/info"+x+" 20 80")
+	def info():	
+		os.system("dialog --no-shadow --menu 'Select Your Choice : ' 20 100 7 1 'Gather information about any website and it(s) owner.' 2 'Know the route to the server where website is hosted.' 3 'Download the Complete Website on your System.' 4 'Back' 2> /root/Desktop/Project/choice.txt")
+		f=open('/root/Desktop/Project/choice.txt','r')
+		c=f.read()
+		if c=='1':
+			os.system("dialog --no-shadow --inputbox 'Enter website name : (Example : google.com) ' 10 50 2> /root/Desktop/Project/hello.txt")
+			f=open('/root/Desktop/Project/hello.txt','r')
+			x=f.read()
+			os.system("dialog --no-shadow --infobox '  Fetching Data.. ' 10 40")
+			os.system("whois -H "+x+" > /root/Desktop/dump/info" +x)
+			os.system("sed -i 's/WHOIS/master/g' /root/Desktop/dump/info"+x)
+			os.system("sed -i 's/whois/master/g' /root/Desktop/dump/info"+x)
+			os.system("dialog --no-shadow --textbox /root/Desktop/dump/info"+x+" 20 80")
+			info()
 
-	if c=='2':
-		os.system("dialog --no-shadow --inputbox 'Enter website name : (Example : google.com) ' 10 50 2> /root/Desktop/Project/hello.txt")
-		f=open('/root/Desktop/Project/hello.txt','r')
-		x=f.read()
-		os.system("dialog --no-shadow --infobox '  Fetching Data.. ' 10 40")
-		os.system("traceroute "+x+" > /root/Desktop/dump/route"+x)
-		os.system("dialog --no-shadow --textbox /root/Desktop/dump/route"+x+" 30 100")
+		if c=='2':
+			os.system("dialog --no-shadow --inputbox 'Enter website name : (Example : google.com) ' 10 50 2> /root/Desktop/Project/hello.txt")
+			f=open('/root/Desktop/Project/hello.txt','r')
+			x=f.read()
+			os.system("dialog --no-shadow --infobox '  Fetching Data.. ' 10 40")
+			os.system("traceroute "+x+" > /root/Desktop/dump/route"+x)
+			os.system("dialog --no-shadow --textbox /root/Desktop/dump/route"+x+" 30 100")
+			info()
+		
+		if c=='3':
+			os.system("dialog --no-shadow --inputbox 'Enter Complete Website Address : (Example : https://www.google.com) ' 10 50 2> /root/Desktop/dump/site")
+			f=open('/root/Desktop/dump/site','r')
+			website=f.read()
+			#os.system("cd /root/Desktop")
+			os.system("dialog --no-shadow --msgbox 'The Website will be mirrored at the current working directory.' 10 50")
+			os.system("dialog --no-shadow --infobox 'The Website Downloading Process Started.. Please wait for some time. \nPress Ctrl + C to stop downloading. ' 10 40")
+			l=commands.getoutput("wget -m -p -E -k -K -np -v "+website+" > /dev/null")
+			os.system("dialog --no-shadow --msgbox 'Website Successfully Captured' 10 50")
+			info()
 
-	if c==3:
-		exit(0)
+
+		if c==4:
+			exit(0)
+
+	info()
+
+a()
